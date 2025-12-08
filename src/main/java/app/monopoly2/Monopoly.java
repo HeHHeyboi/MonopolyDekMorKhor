be1053f8534632d3252234f65e56a20f18d3b920
HeHHeyboi <thanathatwave@gmail.com>
2025-12-08 21:30:10 +0700

    Handle UI update when money change

--- src/main/java/app/monopoly2/Monopoly.java
+++ src/main/java/app/monopoly2/Monopoly.java
@@ -1,14 +1,25 @@
 package app.monopoly2;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.ResourceBundle;
+
 import app.App;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
-public class Monopoly {
+public class Monopoly implements Initializable {
+	@FXML
+	Pane pane;
 	@FXML
 	ImageView background2;
 	@FXML
@@ -29,18 +40,53 @@ public class Monopoly {
 	@FXML
 	Text player4Money;
 
-	public void GameInit(int numplayer) {
+	enum PlayerColor {
+		RED, GREEN, BLUE, YELLOW;
+	}
+
+	final double xSpawn = 721;
+	final double ySpawn = 732;
+	final double playerSize = 10;
+	final int startMoney = 500;
+	ArrayList<Player> players = new ArrayList<>();
+	// ArrayList<Text> nameTexts = new ArrayList<>();
+	ArrayList<Text> moneyTexts = new ArrayList<>();
+
+	static EnumMap<PlayerColor, String> colorMap = new EnumMap<>(PlayerColor.class);
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		if (colorMap.isEmpty()) {
+			colorMap.put(PlayerColor.RED, "#ff0000");
+			colorMap.put(PlayerColor.GREEN, "#03fc03");
+			colorMap.put(PlayerColor.BLUE, "#030bfc");
+			colorMap.put(PlayerColor.YELLOW, "#dbfc03");
+		}
+		// nameTexts.add(player1Name);
+		// nameTexts.add(player2Name);
+		// nameTexts.add(player3Name);
+		// nameTexts.add(player4Name);
+		moneyTexts.add(player1Money);
+		moneyTexts.add(player2Money);
+		moneyTexts.add(player3Money);
+		moneyTexts.add(player4Money);
+	}
+
+	public void GameInit(int numPlayer) {
 		Image image;
-		switch (numplayer) {
+		double startPosY = ySpawn;
+		switch (numPlayer) {
 			case 2:
 				image = new Image("/pic/2Player.png");
 				background2.setImage(image);
+				startPosY -= playerSize * 2;
 				break;
 			case 3:
 				image = new Image("/pic/3Player.png");
 				background2.setImage(image);
 				player3Name.setVisible(true);
 				player3Money.setVisible(true);
+				startPosY -= playerSize * 2 + playerSize;
 				break;
 			case 4:
 				image = new Image("/pic/4Player.png");
@@ -49,7 +95,28 @@ public class Monopoly {
 				player3Money.setVisible(true);
 				player4Name.setVisible(true);
 				player4Money.setVisible(true);
+				startPosY -= playerSize * 4;
 				break;
+		}
+
+		PlayerColor color;
+		for (int i = 0; i < numPlayer; i++) {
+			color = PlayerColor.values()[i];
+			Paint p = Paint.valueOf(colorMap.get(color));
+			Circle c = new Circle(playerSize, p);
+			// Text nText = nameTexts.get(i);
+			// nText.setText(color.toString());
+			// nText.setFill(p);
+			Text mText = moneyTexts.get(i);
+			Player player = new Player(startMoney, c);
+
+			// NOTE: Bind TextProperty from Text to player Money Property
+			mText.textProperty().bind(player.getMoneyProperty().asString());
+
+			c.setLayoutX(xSpawn);
+			c.setLayoutY(startPosY + 20 * i);
+			pane.getChildren().add(player.getPlayer_char());
+			players.addLast(player);
 		}
 	}
 
@@ -62,5 +129,10 @@ public class Monopoly {
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+	}
+
+	public void increaseMoney() {
+		Player p = players.get(1);
+		p.setMoney(p.getMoney() + 10);
 	}
 }
--- src/main/java/app/monopoly2/Player.java
+++ src/main/java/app/monopoly2/Player.java
@@ -1,14 +1,20 @@
 package app.monopoly2;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 
 public class Player {
-	private int money;
+	private IntegerProperty money = new SimpleIntegerProperty();
 	private Circle player_char;
 	private int player_pos = 0;
 
 	public Player(int money, Circle circle) {
-		this.money = money;
+		this.money.set(money);
+		// this.money.addListener((obs, oldvalue, newvalue) -> {
+		// text.setText(newvalue.toString());
+		// });
 		this.player_char = circle;
 	}
 
@@ -21,11 +27,15 @@ public class Player {
 	}
 
 	public int getMoney() {
+		return money.get();
+	}
+
+	public IntegerProperty getMoneyProperty() {
 		return money;
 	}
 
 	public void setMoney(int money) {
-		this.money = money;
+		this.money.set(money);
 	}
 
 	public Circle getPlayer_char() {
